ffcd0834c7b13bca44d79fc5ec4903c7bcc38b17
thecthebest <asyed5839@gmail.com>
2021-07-30 18:07:18 +0200

    Replaced <li> with <TodoItem>
    <ul> tag returns <TodoItem> instead of <li> tag.

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
 //A functional component out of a box can be converted
 //to generic funbction
 
@@ -9,7 +10,7 @@ const Todos: React.FC<{ items: Todo[] }> = (props) => {
     return (
         <ul>
             {props.items.map((item) => {
-                return <li key={item.id}>{item.text}</li>
+                return <TodoItem key={item.id} text={item.text} />
             })}
         </ul>
     );
