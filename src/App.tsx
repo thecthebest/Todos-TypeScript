80204ba689145e86338046e651a3675d0488619c
thecthebest <asyed5839@gmail.com>
2021-07-30 17:48:11 +0200

    Passing down Todos list
    Todos are passed down to the child component via props. "todos" is an array that holds multiple instances of "todo" class.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos = [
+    new Todo("React"),
+    new Todo("typeScript")
+  ];
   return (
     <div className="App">
-      <Todos />
+      <Todos items={todos}/>
     </div>
   );
 }
