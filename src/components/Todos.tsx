09587ad5dd21c2f2ee53564f03d9fdd6c99f5521
thecthebest <asyed5839@gmail.com>
2021-07-30 16:53:27 +0200

    Converted to a functional component
    Previously the function was defined as function declaration, but now has been chnaged to a function expression. The function acts as a functional component. Also <li> tags have been removed because the list will be rendered dynamically.

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,8 +1,12 @@
-function Todos() {
+import React from "react";
+//A functional component out of a box can be converted
+//to generic funbction
+
+//FC = is a function and acts as functional component
+//merge our own object with the base object type(props)      
+const Todos: React.FC<{items: string[]}> = (props) => {
     return (
         <ul>
-            <li>Learn React</li>
-            <li>Learn TypeScript</li>
         </ul>
     );
 }
