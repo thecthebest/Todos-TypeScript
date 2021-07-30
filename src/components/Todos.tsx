64e5f0527fc4617f7dbfa153e0deb03e6173abaf
thecthebest <asyed5839@gmail.com>
2021-07-30 17:39:40 +0200

    Changed the type of props
    Changed from "string[]" to "Todo[]" since "Todo" is a class this lets to set type of the data. In this case the array full of objects.

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import Todo from "../models/todo";
 //A functional component out of a box can be converted
 //to generic funbction
 
 //FC = is a function and acts as functional component
 //merge our own object with the base object type(props)      
-const Todos: React.FC<{ items: string[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
     return (
         <ul>
             {props.items.map((item) => {
-                return <li>{item}</li>
+                return <li>{item.text}</li>
             })}
         </ul>
     );
