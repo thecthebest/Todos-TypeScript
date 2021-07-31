860b99609cc6198cc96dbf2ea2706cedbb969fe9
thecthebest <asyed5839@gmail.com>
2021-07-31 16:44:02 +0200

    Added <NewTodo> component
    The component has been added in the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
@@ -9,7 +10,8 @@ function App() {
   ];
   return (
     <div className="App">
-      <Todos items={todos}/>
+      <NewTodo />
+      <Todos items={todos} />
     </div>
   );
 }
