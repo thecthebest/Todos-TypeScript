57c5512187895ea4acce6098fd9170e7d15bbb9f
thecthebest <asyed5839@gmail.com>
2021-07-31 16:28:20 +0200

    Added "useRef" hook
    The hook has been added to store input value from the user.

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useRef } from "react";
 
 const NewTodo: React.FC<{}> = () => {
+    //Ref is a generic type out of the box
+    //It is required to inform what type of data is stored
+    //use <> to explicitly set the type of ref needed to be created
+    //in this case the type is HTMLInputElement
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
     const submitHandler = (e: React.FormEvent) => {
         e.preventDefault();
     };
     return (
         <form onSubmit={submitHandler}>
             <label htmlFor="text">Todo Text</label>
-            <input type="text" id="text"/>
+            {/*ref out of a box could be assigned to other values null is needed*/}
+            <input ref={todoTextInputRef} type="text" id="text"/>
             <button>Add Todo</button>
         </form>
     );
